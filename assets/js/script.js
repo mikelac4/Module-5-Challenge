d536d0e03f6051cea018e95243e9185c6d3e919c
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-10-18 14:42:41 -0400

    Added first function that gets the values and stores them in the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
         // get nearby values
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id')
+        console.log("this", time)
+        console.log("value", value)
 
         // save in localStorage
+        
 
         // Show notification that item was saved to localStorage by adding class 'show'
 
