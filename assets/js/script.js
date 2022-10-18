372a6aa4ae40d624e36ede169b1eb2725d25d324
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-10-18 15:28:35 -0400

    Added a hour updater function that gets the current number of hours and loops them over time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         console.log("value", value)
 
         // save in localStorage
-        
+        localStorage.setItem(time, value)
 
         // Show notification that item was saved to localStorage by adding class 'show'
 
@@ -21,12 +21,25 @@ $(document).ready(function () {
 
     function hourUpdater() {
         // get current number of hours (preferably with moment.js)
+        var currentHour = moment().hours();
 
         // loop over time blocks
-        // loop over time blocks ---> https://api.jquery.com/each/
-        // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
-
-        // check if we've moved past this time
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+            
+            // check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+              $(this).removeClass('past');
+              $(this).addClass('present');
+            } else {
+              $(this).removeClass('past');
+              $(this).removeClass('present');
+              $(this).addClass('future');  
+            }
+        });
+          
     }
 
     hourUpdater();
@@ -36,8 +49,16 @@ $(document).ready(function () {
 
     // load any saved data from localStorage
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    ///need to repeat line 21 for all the other hours
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
     // display current day on page
+    
 });
